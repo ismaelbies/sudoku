9fe3e432f300fcff2c5233dfaa88e2251533647c
vinicius <vini_colet@hotmail.com>
2022-09-12 19:58:18 -0300

    valida 3x3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
             {4, 9, 6, 1, 8, 2, 5, 7, 3},
             {2, 8, 5, 4, 7, 3, 9, 1, 6}
     };
+
     public static int NUM_THREAD = 11;
     public static boolean[] validaSudoku;
 
@@ -57,25 +58,67 @@ public class Main {
         }
     }
 
+    public static class valida3x3 implements Runnable{
+        int linha;
+        int coluna;
+        int count;
 
+        public valida3x3(int linha, int coluna, int count) {
+            this.linha = linha;
+            this.coluna = coluna;
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            boolean[] validaCelula = new boolean[9];
+            int num;
+            for (int i= linha; i < linha+3; i++){
+                for (int j = coluna; j < coluna+3; j++) {
+
+                    num = matriz[i][j];
+                    if (num < 1 || num > 9 || validaCelula[num - 1]) {
+                        return;
+                    } else if (!validaCelula[num - 1]) {
+                        validaCelula[num - 1] = true;
+                    }
+
+                }
+            }
+            validaSudoku[count] = true;
+        }
+    }
 
     public static void main(String[] args) {
         int threadIndex = 0;
-        Thread threads[] = new Thread[18];
-        validaSudoku = new boolean[18];
+        int count = 18;
+        Thread threads[] = new Thread[27];
+        validaSudoku = new boolean[27];
 
+        //threads das linhas
         for (int i = 0; i < 9; i++){
             threads [threadIndex++] = new Thread(new validaLinha(i));
         }
+        //threads das colunas
         for (int j = 0; j < 9; j++){
             threads [threadIndex++] = new Thread(new validaColuna(j));
         }
+        //threads das matriz 3x3
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                if ( i%3 == 0 && j%3 ==0) {
+                    threads[threadIndex++] = new Thread(new valida3x3(i, j, count++));
+                }
 
+            }
+        }
 
+        //starta todas as thread
         for (int i = 0; i < threads.length; i++){
             threads[i].start();
         }
 
+        //join threads
         for (int i = 0; i < threads.length; i++) {
             try {
                 threads[i].join();
@@ -83,8 +126,11 @@ public class Main {
                 e.printStackTrace();
             }
         }
+
+        //mostra array
         for (int i = 0; i < validaSudoku.length; i++) {
-            System.out.println(validaSudoku[i]);
+            System.out.println( i+ ": " + validaSudoku[i]);
         }
     }
+
 }
